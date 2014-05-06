a4f2abf8f55d0432e00319f19fd99d0c5ac43c89
Vikramsb <vikrambhambere13@gmail.com>
2014-05-06 13:53:38 -0400

    Starter Project
    Hello world Example intro to objects

---
+++ Vsb Project Day2/Vsb Project Day2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Vsb Project Day2
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 Cestar College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
