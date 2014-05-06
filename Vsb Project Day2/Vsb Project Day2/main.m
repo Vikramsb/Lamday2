7401754319dffe9ed4e1f2b16e6aaab17aa20b0d
Vikramsb <vikrambhambere13@gmail.com>
2014-05-06 14:20:30 -0400

    updated code
    Variable assigned to code

--- Vsb Project Day2/Vsb Project Day2/main.m
+++ Vsb Project Day2/Vsb Project Day2/main.m
@@ -12,10 +12,9 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        int numerator = 1;
+        int denominator =3;
+        NSLog(@" The Fraction is %i/%i",numerator,denominator);
     }
     return 0;
 }
