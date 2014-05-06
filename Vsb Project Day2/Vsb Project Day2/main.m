abe2bc7a4d2c94bb2ad6712c252cf2dbf0fde2c8
Vikramsb <vikrambhambere13@gmail.com>
2014-05-06 16:02:50 -0400

    d2

--- Vsb Project Day2/Vsb Project Day2/main.m
+++ Vsb Project Day2/Vsb Project Day2/main.m
@@ -7,14 +7,58 @@
 //
 
 #import <Foundation/Foundation.h>
+// @interface section
+@interface Fraction : NSObject
+
+-(void)print;
+-(void)setNumerator: (int)n;
+-(void)setDenominator:(int)d;
+
+@end
+//implementation section
+@implementation Fraction
+{
+    int Numerator;
+    int Deominator;
+    
+}
+
+-(void)print
+
+{
+    NSLog(@"%i/%i",Numerator,Deominator);
+    
+}
+
+-(void)setNumerator:(int)n
+{
+    Numerator = n;
+}
+
+-(void)setDenominator:(int)d
+
+{
+    
+    Deominator = d;
+}
+
+@end
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        int numerator = 1;
-        int denominator =3;
-        NSLog(@" The Fraction is %i/%i",numerator,denominator);
+        Fraction *myFraction;
+        // Creat an Instance of a fraction
+        myFraction = [Fraction alloc];
+        myFraction = [myFraction init];
+        //set the fraction
+        [myFraction setNumerator:1];
+        [myFraction setDenominator:3];
+        //display
+        
+        NSLog(@" The value of myfraction is :");
+        [myFraction print];
     }
     return 0;
 }
